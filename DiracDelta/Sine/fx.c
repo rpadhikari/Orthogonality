f882863835bacad16a17fa9940cca9e6ec58211c
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-29 19:29:16 +0545

    Add files via upload

--- DiracDelta/Sine/fx.c
+++ DiracDelta/Sine/fx.c
@@ -1,6 +1,10 @@
 double sin(double);
 double fx(int n, double x) {
-  static double pi=3.14159265358979; // pi
-//  static double pi_2=1.772453850905516; //square root of pi
-  return sin((double)n*x)/(pi*x);
+  static double pi=3.14159265358979;
+//  static double pi_2=1.772453850905516;
+  if(x !=0.0) {
+//    return sin(((double)n+0.5)*x)/(2.0*pi*sin(x*0.5));
+    return sin((double)n*x)/(pi*x);
+  }
 }
+
---
+++ DiracDelta/Sine/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+double simp(double, double, int, int);
+int main() {
+  int n;
+  double a, b;
+  static int p=30000;
+  printf(" Value of n = ");
+  scanf("%d", &n);
+  printf(" Lower limit of integral a = ");
+  scanf("%lf",&a);
+  printf(" Lower limit of integral b = ");
+  scanf("%lf",&b);
+  printf(" n = %2d \t Integral = %15.8lf\n", n, simp(a, b, p, n));
+  
+}
+
