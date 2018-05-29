ea6716a86ce3fc9298cf08f994bd83008ce7fee3
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-29 19:22:58 +0545

    Add files via upload

--- DiracDelta/Gauss/fx.c
+++ DiracDelta/Gauss/fx.c
@@ -1,6 +1,7 @@
 double exp(double);
 double fx(int n, double x) {
-//  static double pi=3.14159265358979;  // pi
-  static double pi_2=1.772453850905516; // square root of pi
+//  static double pi=3.14159265358979;
+  static double pi_2=1.772453850905516;
   return (double)n*exp(-(double)n*(double)n*x*x)/pi_2;
 }
+
