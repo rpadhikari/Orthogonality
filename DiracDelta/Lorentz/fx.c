cdb813e38a6168830c74da3f9da156731b8214cb
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-29 19:31:05 +0545

    Update fx.c

--- DiracDelta/Lorentz/fx.c
+++ DiracDelta/Lorentz/fx.c
@@ -1,6 +1,6 @@
 double fx(int n, double x) {
   static double pi=3.14159265358979;
-//  static double pi_2=1.772453850905516;
+//  static double pi_2=1.772453850905516; // pi/2 in case I need.
   if(x != 0.0) {
     return ((double)n/pi)/(1.0+(double)n*(double)n*x*x);
   }
