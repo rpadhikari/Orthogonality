9bc00f815b48f3bcde8d3b543edbaa429bd47c0b
Rajendra Adhikari <rpadhikari@gmail.com>
2018-05-29 19:27:45 +0545

    Add files via upload

--- DiracDelta/Lorentz/fx.c
+++ DiracDelta/Lorentz/fx.c
@@ -1,5 +1,8 @@
 double fx(int n, double x) {
-  static double pi=3.14159265358979; // pi
-//  static double pi_2=1.772453850905516; // square root of pi
-  return ((double)n/pi)/(1.0+(double)n*(double)n*x*x);
+  static double pi=3.14159265358979;
+//  static double pi_2=1.772453850905516;
+  if(x != 0.0) {
+    return ((double)n/pi)/(1.0+(double)n*(double)n*x*x);
+  }
 }
+
